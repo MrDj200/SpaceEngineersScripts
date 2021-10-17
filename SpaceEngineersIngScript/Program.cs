d745436d4f821a298dbd683d8c2ea9bd798f0e92
MrDj200 <david@klatt-luedenscheid.de>
2021-10-17 15:54:15 +0200

    White space cleanup

--- SpaceEngineersIngScript/Program.cs
+++ SpaceEngineersIngScript/Program.cs
@@ -1,175 +1,165 @@
-﻿using Sandbox.Game.EntityComponents;
-using Sandbox.ModAPI.Ingame;
-using Sandbox.ModAPI.Interfaces;
-using SpaceEngineers.Game.ModAPI.Ingame;
-using System.Collections.Generic;
-using System.Collections;
-using System.Linq;
-using System.Text;
+﻿using Sandbox.ModAPI.Ingame;
 using System;
-using VRage.Collections;
-using VRage.Game.Components;
+using System.Collections.Generic;
+using System.Text;
 using VRage.Game.ModAPI.Ingame;
-using VRage.Game.ObjectBuilders.Definitions;
-using VRage.Game;
-using VRageMath;
 
 namespace IngameScript
 {
-	partial class Program : MyGridProgram
-	{
+    partial class Program : MyGridProgram
+    {
 
 
-		void Configuration()
-		{
-			//Name of the Target Container where the Assemblers Items will be moved to:
-			DjConfig.targetContainerName = "AssContainer";
+        void Configuration()
+        {
+            //Name of the Target Container where the Assemblers Items will be moved to:
+            DjConfig.targetContainerName = "AssContainer";
 
-			//Name of the Main Storage the Items will be moved when the above is to be cleared:
-			DjConfig.mainCargoName = "Large Cargo Container (Master)";
+            //Name of the Main Storage the Items will be moved when the above is to be cleared:
+            DjConfig.mainCargoName = "Large Cargo Container (Master)";
 
-			DjConfig.disassemblerNameTag = "[DjDis]";
-			
-		}
+            DjConfig.disassemblerNameTag = "[DjDis]";
+
+        }
 
 
-		///////////////////////////////
-		//Do not modify the following//
-		///////////////////////////////
+        ///////////////////////////////
+        //Do not modify the following//
+        ///////////////////////////////
 
-		public static class DjConfig
-		{
-			public static String targetContainerName = "AssCargo";
-			public static String mainCargoName = "MainCargo";
-			public static String disassemblerNameTag = "[DjDis]";
-			public static IMyCargoContainer targetContainer, mainCargo;
-			public static IMyInventory targetInv, mainInv;
-		}
+        public static class DjConfig
+        {
+            public static String targetContainerName = "AssCargo";
+            public static String mainCargoName = "MainCargo";
+            public static String disassemblerNameTag = "[DjDis]";
+            public static IMyCargoContainer targetContainer, mainCargo;
+            public static IMyInventory targetInv, mainInv;
+        }
 
-		List<IMyAssembler> allAsses = new List<IMyAssembler>();
-		List<IMyAssembler> disassemblers = new List<IMyAssembler>();
+        List<IMyAssembler> allAsses = new List<IMyAssembler>();
+        List<IMyAssembler> disassemblers = new List<IMyAssembler>();
 
-		public Program()
-		{
-			Configuration();
-			Runtime.UpdateFrequency = UpdateFrequency.Update100;
+        public Program()
+        {
+            Configuration();
+            Runtime.UpdateFrequency = UpdateFrequency.Update100;
 
-			DjConfig.targetContainer = (IMyCargoContainer)GridTerminalSystem.GetBlockWithName(DjConfig.targetContainerName);
-			DjConfig.targetInv = DjConfig.targetContainer.GetInventory();
+            DjConfig.targetContainer = (IMyCargoContainer)GridTerminalSystem.GetBlockWithName(DjConfig.targetContainerName);
+            DjConfig.targetInv = DjConfig.targetContainer.GetInventory();
 
-			DjConfig.mainCargo = (IMyCargoContainer)GridTerminalSystem.GetBlockWithName(DjConfig.mainCargoName);
-			DjConfig.mainInv = DjConfig.mainCargo.GetInventory();			
+            DjConfig.mainCargo = (IMyCargoContainer)GridTerminalSystem.GetBlockWithName(DjConfig.mainCargoName);
+            DjConfig.mainInv = DjConfig.mainCargo.GetInventory();
 
-		}
+        }
 
-		public void Main(string argument, UpdateType updateSource)
-		{
-			RunStuff();
+        public void Main(string argument, UpdateType updateSource)
+        {
+            RunStuff();
 
-			GridTerminalSystem.GetBlocksOfType(disassemblers, block =>
-				block.CustomName.Contains(DjConfig.disassemblerNameTag) &&
-				block.IsWorking &&
-				block.CubeGrid == Me.CubeGrid
-			);
-			
-			if (disassemblers.Count > 0)
-			{
-				RunDisassemblerStuff();
-			}
+            GridTerminalSystem.GetBlocksOfType(disassemblers, block =>
+                block.CustomName.Contains(DjConfig.disassemblerNameTag) &&
+                block.IsWorking &&
+                block.CubeGrid == Me.CubeGrid
+            );
 
-			Echo($"Found {allAsses.Count} Assemblers\nFound {disassemblers.Count} Disassemblers");
+            if (disassemblers.Count > 0)
+            {
+                RunDisassemblerStuff();
+            }
 
-			if (updateSource == UpdateType.Trigger)
-			{
-				if (argument.StartsWith("clear"))
-				{
-					ClearContainer(DjConfig.targetInv, DjConfig.mainInv);
-					return;
-				}
-			}
-					
-		}
+            Echo($"Found {allAsses.Count} Assemblers\nFound {disassemblers.Count} Disassemblers");
 
-		void RunDisassemblerStuff()
-		{			
-			disassemblers.ForEach(disAss =>
-			{
-				disAss.Mode = MyAssemblerMode.Disassembly;
-				disAss.Repeating = true;
-			});			
+            if (updateSource == UpdateType.Trigger)
+            {
+                if (argument.StartsWith("clear"))
+                {
+                    ClearContainer(DjConfig.targetInv, DjConfig.mainInv);
+                    return;
+                }
+            }
 
-			IMyInventory testInv = disassemblers[0].GetInventory(1);
+        }
 
-			List<IMyTerminalBlock> connectedBlocks = new List<IMyTerminalBlock>();
-			GridTerminalSystem.GetBlocksOfType(connectedBlocks, block =>
-				block.HasInventory &&
-				block.IsWorking &&
-				block.GetInventory(0).IsConnectedTo(testInv)
-			);
+        void RunDisassemblerStuff()
+        {
+            disassemblers.ForEach(disAss =>
+            {
+                disAss.Mode = MyAssemblerMode.Disassembly;
+                disAss.Repeating = true;
+            });
 
-			StringBuilder msg = new StringBuilder();
+            IMyInventory testInv = disassemblers[0].GetInventory(1);
 
-			connectedBlocks.ForEach(block =>
-			{
-				List<MyInventoryItem> items = new List<MyInventoryItem>();
-				block.GetInventory(0).GetItems(items, item =>
-					item.Type == MyItemType.MakeTool("WelderItem") ||
-					item.Type == MyItemType.MakeTool("AngleGrinderItem") ||
-					item.Type == MyItemType.MakeTool("HandDrillItem") ||
-					item.Type == MyItemType.MakeTool("AutomaticRifleItem") 
-				);
+            List<IMyTerminalBlock> connectedBlocks = new List<IMyTerminalBlock>();
+            GridTerminalSystem.GetBlocksOfType(connectedBlocks, block =>
+                block.HasInventory &&
+                block.IsWorking &&
+                block.GetInventory(0).IsConnectedTo(testInv)
+            );
 
-				//block.GetInventory(0).GetItems(items);
+            StringBuilder msg = new StringBuilder();
 
-				if (items.Count > 0)
-				{
-					items.ForEach(item =>
-					{
-						block.GetInventory(0).TransferItemTo(testInv, item);
-					});
-				}
-								
-			});
-			
-		}
+            connectedBlocks.ForEach(block =>
+            {
+                List<MyInventoryItem> items = new List<MyInventoryItem>();
+                block.GetInventory(0).GetItems(items, item =>
+                    item.Type == MyItemType.MakeTool("WelderItem") ||
+                    item.Type == MyItemType.MakeTool("AngleGrinderItem") ||
+                    item.Type == MyItemType.MakeTool("HandDrillItem") ||
+                    item.Type == MyItemType.MakeTool("AutomaticRifleItem")
+                );
 
-		void RunStuff()
-		{
-			allAsses.Clear();
-			GridTerminalSystem.GetBlocksOfType(allAsses, ass =>
-				ass.UseConveyorSystem &&
-				ass.Mode == MyAssemblerMode.Assembly &&
-				ass.IsWorking &&
-				ass.GetInventory(1).IsConnectedTo(DjConfig.targetInv) &&
-				ass.CubeGrid == Me.CubeGrid
-			);
-			foreach (IMyAssembler ass in allAsses)
-			{
-				ass.GetInventory(1).TransferItemTo(DjConfig.targetInv, 0);
-			}
-		}
+                //block.GetInventory(0).GetItems(items);
 
-		void ClearContainer(IMyInventory sourceContainer, IMyInventory targetContainer)
-		{
-			if (sourceContainer == null || targetContainer == null)
-			{
-				Echo("One of the Containers could not be found!");
-				return;
-			}
-			if (!sourceContainer.IsConnectedTo(targetContainer))
-			{
-				Echo("The Source Container is not Connected to the Target Container! (Check the Conveyor Lines)");
-				return;
-			}
-			List<MyInventoryItem> myItems = new List<MyInventoryItem>();
-			sourceContainer.GetItems(myItems);
+                if (items.Count > 0)
+                {
+                    items.ForEach(item =>
+                    {
+                        block.GetInventory(0).TransferItemTo(testInv, item);
+                    });
+                }
 
-			int count = myItems.Count;
-			for (int i = 0; i <= count; i++)
-			{
-				sourceContainer.TransferItemTo(targetContainer, 0);
-			}
-		}
+            });
 
-	}
+        }
+
+        void RunStuff()
+        {
+            allAsses.Clear();
+            GridTerminalSystem.GetBlocksOfType(allAsses, ass =>
+                ass.UseConveyorSystem &&
+                ass.Mode == MyAssemblerMode.Assembly &&
+                ass.IsWorking &&
+                ass.GetInventory(1).IsConnectedTo(DjConfig.targetInv) &&
+                ass.CubeGrid == Me.CubeGrid
+            );
+            foreach (IMyAssembler ass in allAsses)
+            {
+                ass.GetInventory(1).TransferItemTo(DjConfig.targetInv, 0);
+            }
+        }
+
+        void ClearContainer(IMyInventory sourceContainer, IMyInventory targetContainer)
+        {
+            if (sourceContainer == null || targetContainer == null)
+            {
+                Echo("One of the Containers could not be found!");
+                return;
+            }
+            if (!sourceContainer.IsConnectedTo(targetContainer))
+            {
+                Echo("The Source Container is not Connected to the Target Container! (Check the Conveyor Lines)");
+                return;
+            }
+            List<MyInventoryItem> myItems = new List<MyInventoryItem>();
+            sourceContainer.GetItems(myItems);
+
+            int count = myItems.Count;
+            for (int i = 0; i <= count; i++)
+            {
+                sourceContainer.TransferItemTo(targetContainer, 0);
+            }
+        }
+
+    }
 }
